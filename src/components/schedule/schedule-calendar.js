80f75bf0dedb771887d94be965bc59ff700a20d7
Kaiyuan Li <A01038135@ad.bcit.ca>
2019-10-23 09:59:56 -0700

    Finish page-schedule

--- src/components/schedule/schedule-calendar.js
+++ src/components/schedule/schedule-calendar.js
@@ -1,15 +1,85 @@
 import { LitElement, html, css } from 'lit-element'
+import { schedule } from '../../config'
 
 export default class ScheduleCalendar extends LitElement {
   render () {
-    return html`
-    
-    `
+    const rows = schedule[this.set].reduce((prev, next, i) => {
+      if (!(prev[0] instanceof Array)) {
+        prev = [[prev]]
+      }
+      if (i % 6 === 0) {
+        prev.push([])
+      }
+      prev[prev.length - 1].push(next)
+      return prev
+    })
+    for (const row of rows) {
+      for (let i = 0; i < 6; i++) {
+        if (!row[i]) {
+          row[i] = []
+        }
+      }
+    }
+    switch (this.view) {
+      case 'calendar':
+        return html`
+          <table class="calendar">
+            <thead>
+              <tr>
+                <th></th>
+                <th>Monday</th>
+                <th>Tuesday</th>
+                <th>Wednesday</th>
+                <th>Thursday</th>
+                <th>Friday</th>
+              </tr>
+            </thead>
+            <tbody>
+              ${rows.map(row => {
+                return html`
+                  <tr>
+                    ${row.map(col => html`<td>${col.map(line => line && html`<span>${line}</span><br />`)}</td>`)}
+                  </tr>
+                `
+              })}
+            </tbody>
+          </table>
+        `
+      case 'list':
+        return html`
+          <table class="list">
+            ${schedule[this.set].map(row => {
+              return html`
+                <tr>
+                  <td>${row.map(line => line && html`<span>${line}</span><br />`)}</td>
+                </tr>
+              `
+            })}
+          </table>
+        `
+    }
   }
 
   static get styles () {
     return css`
-
+      table {
+        border-spacing: 0;
+      }
+      table.list {
+        width: 100%;
+        max-width: 800px;
+      }
+      th, td {
+        vertical-align: top;
+        border: 1px solid #777;
+        padding: 5px;
+      }
+      table.calendar td {
+        height: 90px;
+      }
+      td span:first-child {
+        font-weight: bold;
+      }
     `
   }
 
--- src/pages/page-schedule.js
+++ src/pages/page-schedule.js
@@ -9,7 +9,6 @@ export default class PageSchedule extends LitElement {
   render () {
     return html`
       <page-content>
-        <schedule-calendar set="setA" view="calendar"></schedule-calendar>
         <mwc-formfield label="Set A">
           <mwc-radio name="set" value="setA" @change=${this.changeSet.bind(this)} checked></mwc-radio>
         </mwc-formfield>
@@ -17,20 +16,47 @@ export default class PageSchedule extends LitElement {
           <mwc-radio name="set" value="setB" @change=${this.changeSet.bind(this)}></mwc-radio>
         </mwc-formfield>
         <mwc-formfield label="Calendar view">
-          <mwc-checkbox checked></mwc-checkbox>
+          <mwc-checkbox checked @change=${this.toggleCalendarView.bind(this)}></mwc-checkbox>
         </mwc-formfield>
+        <schedule-calendar set="setA" view="calendar"></schedule-calendar>
       </page-content>
     `
   }
 
   static get styles () {
     return css`
+      @media screen and (max-width: 949px) {
+        mwc-formfield[label="Calendar view"] {
+          display: none;
+        }
+      }
     `
   }
 
   changeSet (event) {
     if (event.target.checked) {
       this.shadowRoot.querySelector('schedule-calendar').set = event.target.value
+    }
+  }
+
+  toggleCalendarView (event) {
+    this.shadowRoot.querySelector('schedule-calendar').view = event.target.checked ? 'calendar' : 'list'
+  }
+
+  connectedCallback () {
+    super.connectedCallback()
+    window.addEventListener('resize', this.resize.bind(this))
+  }
+
+  disconnectedCallback () {
+    super.disconnectedCallback()
+    window.removeEventListener('resize')
+  }
+
+  resize () {
+    if (window.innerWidth < 950) {
+      this.shadowRoot.querySelector('mwc-checkbox').checked = false
+      this.shadowRoot.querySelector('mwc-checkbox').dispatchEvent(new window.Event('change'))
     }
   }
 }
